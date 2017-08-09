d0dc550a9f2c6b5a4b57fd6da497c1e126493b58
Chris Smith <42@chrisryansmith.com>
2017-08-09 10:44:54 +0200

    setup mergeSort scaffold

---
+++ merge/mergeSort.js
@@ -0,0 +1,13 @@
+const pry = require('pryjs')
+
+function mergeSort(unsorted) {
+  this.unsorted = unsorted,
+  this.run = () => {
+    
+  },
+  this.compareArrays = () => {}
+}
+
+module.exports = mergeSort
+
+console.log(new mergeSort([ 5, 4, 3, 2, 1 ]).run())
---
+++ merge/test/mergeSort-test.js
@@ -0,0 +1,33 @@
+const assert = require('chai').assert
+const sorter = require('../mergeSort.js')
+
+describe('bubbleSort', () => {
+  context('when sorted', () => {
+    it('it returns original array of letters', () => {
+      const results = new sorter(['a', 'b']).run()
+      assert.deepEqual(results, ['a', 'b'])
+    })
+
+    it('it returns original array of numbers', () => {
+      const results = new sorter([0, 1]).run()
+      assert.deepEqual(results, [0, 1])
+    })
+  })
+
+  context('when out of order', () => {
+    it('it can sort a two element array', () => {
+      const results = new sorter(['b', 'a']).run()
+      assert.deepEqual(results, ['a', 'b'])
+    })
+
+    it('it can sort 4 letters', () => {
+      const results = new sorter(['b', 'a', 'c', 'd']).run()
+      assert.deepEqual(results, ['a', 'b', 'c', 'd'])
+    })
+
+    it('it can sort 4 numbers', () => {
+      const results = new sorter([4,3,2,1]).run()
+      assert.deepEqual(results, [1,2,3,4])
+    })
+  })
+})
